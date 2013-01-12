c331fc53e164f9fe19078c9643f934ec1e40687f
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2013-01-12 07:53:45 -0500

    name changes to `private` variables and methods to prevent overriding

--- state-machine.js
+++ state-machine.js
@@ -4,49 +4,49 @@ Array.prototype.push = function(obj) { this[this.length] = obj }
 var StateMachine = function(functions, transitions, initial) {
 	var initial = initial || 'initial'
 
-	this.slice = Array.prototype.slice
-	this.running = 0
-	this.stack = []
-	this.transitions = {}
+	this._sm_slice = Array.prototype.slice
+	this._sm_running = 0
+	this._sm_stack = []
+	this._sm_transitions = {}
 
 	// organize transitions to this.transitions
-	this._createTransitions(transitions)
+	this._sm_createTransitions(transitions)
 
 	// add functions to the object
-	this._createFunctions(functions)
+	this._sm_createFunctions(functions)
 
 	this.state = initial
 }
 
 StateMachine.prototype = {
 	on: function(event, handler) {
-		this.handlers = this.handlers || {}
-		this.handlers[event] = this.handlers[event] || []
-		this.handlers[event].push(handler)
+		this._sm_handlers = this._sm_handlers || {}
+		this._sm_handlers[event] = this._sm_handlers[event] || []
+		this._sm_handlers[event].push(handler)
 	},
 	trigger: function(event /*, args */) {
-		this.handlers = this.handlers || {}
-		for(var i in this.handlers[event]) {
-			this.handlers[event][i].apply(this, this.slice.call(arguments, 1))
+		this._sm_handlers = this._sm_handlers || {}
+		for(var i in this._sm_handlers[event]) {
+			this._sm_handlers[event][i].apply(this, this._sm_slice.call(arguments, 1))
 		}
 	},
 	off: function(event, handler) {
 		//remove all
 		if(event == null) {
 			var hasRemoved = false
-			for(var eventName in this.handlers) {
-				if(this.handlers[eventName].length > 0) {
-					this.handlers[eventName].length = 0
+			for(var eventName in this._sm_handlers) {
+				if(this._sm_handlers[eventName].length > 0) {
+					this._sm_handlers[eventName].length = 0
 					hasRemoved = true
 				}
 			}
 			return hasRemoved
 		}
-		this.handlers = this.handlers || {}
+		this._sm_handlers = this._sm_handlers || {}
 		// remove all for event
 		if(handler == null) {
-			if(this.handlers[event] != null) {
-				this.handlers[event].length = 0
+			if(this._sm_handlers[event] != null) {
+				this._sm_handlers[event].length = 0
 				return true
 			}
 			else {
@@ -55,9 +55,9 @@ StateMachine.prototype = {
 		}
 		// remove handler for event
 		var hasRemoved = false
-		for(var i in this.handlers[event]) {
-			if(handler == this.handlers[event][i]) {
-				this.handlers[event].splice(i, 1)
+		for(var i in this._sm_handlers[event]) {
+			if(handler == this._sm_handlers[event][i]) {
+				this._sm_handlers[event].splice(i, 1)
 				hasRemoved = true
 			}
 		}
@@ -66,46 +66,46 @@ StateMachine.prototype = {
 
 
 	// private
-	_createTransitions: function(transitions) {
-		var length = transitions.length, t
-		for(var i=0; i<length; i++) {
+	_sm_createTransitions: function(transitions) {
+		var t
+		for(var i=0; i<transitions.length; i++) {
 			t = transitions[i]
-			this._createTransition(t[0], t[1], t[2])
+			this._sm_createTransition(t[0], t[1], t[2])
 		}
 	},
-	_createTransition: function(fromState, methods, toState) {
-		this.transitions[fromState] = this.transitions[fromState] || []
-		this.transitions[fromState].push({
+	_sm_createTransition: function(fromState, methods, toState) {
+		this._sm_transitions[fromState] = this._sm_transitions[fromState] || []
+		this._sm_transitions[fromState].push({
 			steps: methods,
 			toState: toState
 		})
 	},
-	_createFunctions: function(functions) {
+	_sm_createFunctions: function(functions) {
 		for(var funcName in functions) {
-			this[funcName] = this._createFunction(funcName, functions[funcName])
+			this[funcName] = this._sm_createFunction(funcName, functions[funcName])
 		}
 	},
-	_createFunction: function(funcName, func) {
+	_sm_createFunction: function(funcName, func) {
 		return function() {
-			this.running++
-			func.apply(this, this.slice.call(arguments))
-			this.running--
-			var transition = this._getTransition(funcName) 
+			this._sm_running++
+			func.apply(this, this._sm_slice.call(arguments))
+			this._sm_running--
+			var transition = this._sm_getTransition(funcName) 
 			if(transition != null) {
 				this.state = transition.toState
 				this.trigger(this.state)
 			}
 		}
 	},
-	_getTransition: function(funcName) {
+	_sm_getTransition: function(funcName) {
 		var transition = null
-		if(!this.running) {
-			var stack = this.stack,
-				transitions = this.transitions[this.state], 
+		if(!this._sm_running) {
+			var stack = this._sm_stack,
+				transitions = this._sm_transitions[this.state], 
 				length = transitions && transitions.length || 0
 
 			if(length) {
-				this.stack.push(funcName)
+				this._sm_stack.push(funcName)
 			}
 			for(var i=0; i<length; i++) {
 				transition = transitions[i]
@@ -114,7 +114,7 @@ StateMachine.prototype = {
 				})
 
 				if(canTransition) {
-					this.stack.length = 0
+					this._sm_stack.length = 0
 					break
 				}
 				else {
